b8aa58fd1fb97c48da01d8cc5f02a42249813345
Junior Ales <edilson.ales.jr@gmail.com>
2016-08-07 22:17:53 +0100

    State to start every solution
    This reverts commit 3d014dcb017e949eddc6c0415ff5345dd1955d50.

--- app/views/index.js
+++ app/views/index.js
@@ -1,45 +1,37 @@
-const player1Name = 'Player 1';
-const player2Name = 'Player 2';
-let player1Score = 0;
-let player2Score = 0;
+const init = function() {
+  const $page = Object.freeze({
+    player1: {
+      name: document.querySelector('[data-player1-name]'),
+      score: document.querySelector('[data-player1-score]')
+    },
+    player2: {
+      name: document.querySelector('[data-player2-name]'),
+      score: document.querySelector('[data-player2-score]')
+    },
+    gameScore: document.querySelector('[data-parsed-score]'),
 
-const getScore = function() {
-  let s;
-  if ((player1Score < 4 && player2Score < 4) && (player1Score + player2Score < 6)) {
-    var p = ['Love', 'Fifteen', 'Thirty', 'Forty'];
-    s = p[player1Score];
-    return (player1Score === player2Score) ? s + '-All' : s + '-' + p[player2Score];
-  } else {
-    if (player1Score === player2Score)
-      return 'Deuce';
-    s = player1Score > player2Score ? player1Name : player2Name;
-    return ((player1Score - player2Score) * (player1Score - player2Score) === 1) ? 'Advantage ' + s : 'Win for ' + s;
-  }
+    render(game) {
+      this.player1.name.textContent = game.player1.name;
+      this.player1.score.textContent = game.player1.score;
+      this.player2.name.textContent = game.player2.name;
+      this.player2.score.textContent = game.player2.score;
+      this.gameScore.textContent = game.gameScore;
+    }
+  });
+
+  const game = {
+    player1: {
+      name: 'Djokovic',
+      score: 2
+    },
+    player2: {
+      name: 'Nadal',
+      score: 1
+    },
+    gameScore: 'Thirty-Fifteen'
+  };
+
+  $page.render(game);
 };
 
-document.querySelector('[data-player1-name]').textContent = player1Name;
-document.querySelector('[data-player1-score]').textContent = player1Score;
-document.querySelector('[data-player2-name]').textContent = player2Name;
-document.querySelector('[data-player2-score]').textContent = player2Score;
-document.querySelector('[data-parsed-score]').textContent = getScore();
-
-document.querySelector('[data-player1-add-score]').addEventListener('click', () => {
-  player1Score++;
-  document.querySelector('[data-parsed-score]').textContent = getScore();
-  document.querySelector('[data-player1-score]').textContent = player1Score;
-});
-
-document.querySelector('[data-player2-add-score]').addEventListener('click', () => {
-  player2Score++;
-  document.querySelector('[data-parsed-score]').textContent = getScore();
-  document.querySelector('[data-player2-score]').textContent = player2Score;
-});
-
-document.querySelector('[data-score-reset]').addEventListener('click', () => {
-  player1Score = 0;
-  player2Score = 0;
-
-  document.querySelector('[data-parsed-score]').textContent = getScore();
-  document.querySelector('[data-player1-score]').textContent = player1Score;
-  document.querySelector('[data-player2-score]').textContent = player2Score;
-});
+document.addEventListener('DOMContentLoaded', init);
