41245066cf28206704c7d187175fb06ea75d8f6a
Junior Ales <edilson.ales.jr@gmail.com>
2016-08-07 05:13:25 +0100

    adding frontend markup

--- app/views/components/greetingView.js
+++
@@ -1,7 +0,0 @@
-const greetingView = {
-  render() {
-    return '<h1 class="greeting-panel">Hi! I am your first raw component.</h1>';
-  }
-};
-
-module.exports = greetingView;
--- spec/views/components/greetingViewSpec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const GreetingView = require('../../../app/views/components/greetingView');
-
-describe('Greeting View', () => {
-  it('should mention it is a raw HTML component', () => {
-    expect(GreetingView.render()).toContainString('raw component');
-  });
-});
