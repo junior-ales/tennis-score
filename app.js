bfd430e2024cc4b864fbd689adf4b8a39ab02251
Junior Ales <edilson.ales.jr@gmail.com>
2016-08-12 18:01:56 +0100

    changing implementation to express data hiding concept

--- app.js
+++ app.js
@@ -12,86 +12,114 @@
       resetScore: function() { document.location.reload(); }
     };
 
-    return {
+    var _game, _domElems;
+
+    var _render = function() {
+      _domElems.$p1Name.textContent = _game.getPlayer1().getName();
+      _domElems.$p1Score.textContent = _game.getPlayer1().getScore();
+      _domElems.$p2Name.textContent = _game.getPlayer2().getName();
+      _domElems.$p2Score.textContent = _game.getPlayer2().getScore();
+      _domElems.$gameScore.textContent = _game.getScore();
+    };
+
+    var _p1Scored = function() {
+      _game.getPlayer1().scored();
+      _render();
+    };
+
+    var _p2Scored = function() {
+      _game.getPlayer2().scored();
+      _render();
+    };
+
+    var _bindActions = function() {
+      _domElems.$p1AddScore.addEventListener('click', _p1Scored);
+      _domElems.$p2AddScore.addEventListener('click', _p2Scored);
+      _domElems.$resetScore.addEventListener('click', _domElems.resetScore);
+    };
+
+    var publicApi = {
       init: function(game, domElems) {
-        this.game = game;
-        this.domElems = domElems || DEFAULT_DOM_ELEMS;
+        _game = game;
+        _domElems = domElems || DEFAULT_DOM_ELEMS;
 
-        this.bindActions();
-        this.render();
-      },
-
-      p1Scored: function() {
-        this.game.player1.scored();
-        this.render();
-      },
-
-      p2Scored: function() {
-        this.game.player2.scored();
-        this.render();
-      },
-
-      bindActions: function() {
-        this.domElems.$p1AddScore.addEventListener('click', this.p1Scored.bind(this));
-        this.domElems.$p2AddScore.addEventListener('click', this.p2Scored.bind(this));
-        this.domElems.$resetScore.addEventListener('click', this.domElems.resetScore);
-      },
-
-      render: function() {
-        this.domElems.$p1Name.textContent = this.game.player1.name;
-        this.domElems.$p1Score.textContent = this.game.player1.score;
-        this.domElems.$p2Name.textContent = this.game.player2.name;
-        this.domElems.$p2Score.textContent = this.game.player2.score;
-        this.domElems.$gameScore.textContent = this.game.getScore();
+        _bindActions();
+        _render();
       }
     };
+
+    return Object.freeze(publicApi);
   })();
 
-  var Player = {
-    init: function(name) {
-      this.name = name;
-      this.score = 0;
-      return this;
-    },
+  var Player = (function() {
+    var publicApi = {
+      init: function(name) {
+        var _name = name;
+        var _score = 0;
 
-    scored: function() {
-      this.score++;
-    }
-  };
+        this.getName = function() {
+          return _name;
+        };
 
-  var Game = {
-    init: function(player1name, player2name) {
-      this.player1 = Object.create(Player).init(player1name);
-      this.player2 = Object.create(Player).init(player2name);
-      return this;
-    },
+        this.getScore = function() {
+          return _score;
+        };
 
-    wonPoint: function(playerName) {
-      if (this.player1.name === playerName) this.player1.scored();
-      if (this.player2.name === playerName) this.player2.scored();
-    },
+        this.scored = function() { _score++; };
 
-    getScore: function() {
-      var SCORES = ['Love', 'Fifteen', 'Thirty', 'Forty'];
-      var DRAW_SCORES = ['Love-All', 'Fifteen-All', 'Thirty-All'];
-      var scoreDiff = this.player1.score - this.player2.score;
+        return this;
+      }
+    };
+
+    return Object.freeze(publicApi);
+  })();
+
+  var Game = (function() {
+    var SCORES = ['Love', 'Fifteen', 'Thirty', 'Forty'];
+    var DRAW_SCORES = ['Love-All', 'Fifteen-All', 'Thirty-All'];
+
+    var _player1, _player2;
+
+    var _getScore = function() {
+      var scoreDiff = _player1.getScore() - _player2.getScore();
 
       var isDraw = scoreDiff === 0;
       if (isDraw) {
-        return DRAW_SCORES[this.player1.score] || 'Deuce';
+        return DRAW_SCORES[_player1.getScore()] || 'Deuce';
       }
 
-      var isHighScore = this.player1.score > 3 || this.player2.score > 3;
+      var isHighScore = _player1.getScore() > 3 || _player2.getScore() > 3;
       if (isHighScore) {
-        if (scoreDiff ===  1) return 'Advantage ' + this.player1.name;
-        if (scoreDiff === -1) return 'Advantage ' + this.player2.name;
+        if (scoreDiff ===  1) return 'Advantage ' + _player1.getName();
+        if (scoreDiff === -1) return 'Advantage ' + _player2.getName();
 
-        return 'Win for ' + (scoreDiff > 1 ? this.player1.name : this.player2.name);
+        return 'Win for ' + (scoreDiff > 1 ? _player1.getName() : _player2.getName());
       }
 
-      return SCORES[this.player1.score] + '-' + SCORES[this.player2.score];
-    }
-  };
+      return SCORES[_player1.getScore()] + '-' + SCORES[_player2.getScore()];
+    };
+
+    var _wonPoint = function(playerName) {
+      if (_player1.getName() === playerName) _player1.scored();
+      if (_player2.getName() === playerName) _player2.scored();
+    };
+
+    var publicApi = {
+      init: function(player1name, player2name) {
+        _player1 = Object.create(Player).init(player1name);
+        _player2 = Object.create(Player).init(player2name);
+
+        this.getPlayer1 = function() { return _player1; };
+        this.getPlayer2 = function() { return _player2; };
+        this.getScore = _getScore;
+        this.wonPoint = _wonPoint;
+
+        return this;
+      }
+    };
+
+    return Object.freeze(publicApi);
+  })();
 
   var init = function() {
     var game = Object.create(Game).init('Serena', 'Kerber');
